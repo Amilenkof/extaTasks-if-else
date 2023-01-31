3a4af0e4495cb850653679c9b8af55a04d826d65
Amilenkof <f.o.g@list.ru>
2023-01-31 17:55:46 +0400

    2 task is done

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int age= 9;
-        double salary= 20000;
-        double creditLimit ;
-        if (age>=23) {creditLimit=salary*3;}
-        else {creditLimit=salary*2;}
+        int age =32;
+        double salary= 51_000;
+        double wantedSum=300_000;
+        double bidBase=0.1;
+        int durationCredit=12;
+        double bidClient=bidBase;
 
-        if (salary>=50_000 && salary<80_000) {creditLimit=creditLimit*1.2;}
+        if (age<23) {
+            bidClient=bidBase+0.01;
+        }
+        else if (age<30&&age>=23) {
+            bidClient=bidBase+0.05;
+        }
 
-         else if  (salary>80_000){ creditLimit=creditLimit*1.3;}
+        else if (salary>80_000) {
+            bidClient=bidBase-0.7;}
 
-        System.out.println("кредитный лимит"+creditLimit);
-    }}
+
+        double maxPay=(wantedSum*bidClient+wantedSum)/durationCredit;
+            System.out.println(maxPay);
+
+        if (maxPay>salary/2) {
+            System.out.println("Максимальный платеж при ЗП "+salary+"равен "+maxPay+"рублей. Платеж по кредиту "+wantedSum+".Отказано ");
+        }else {
+            System.out.println("Максимальный платеж при ЗП " + salary + "равен " + maxPay + "рублей. Платеж по кредиту " + wantedSum + ".Одобрено ");
+    }}}
